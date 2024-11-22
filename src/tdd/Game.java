815d9bc3ad0be404f1b0d4217353b2a654b8f22e
TravailAuBlack <yassine82600@gmail.com>
2024-11-22 12:01:20 +0100

    quilles

--- src/tdd/Game.java
+++ src/tdd/Game.java
@@ -1,5 +1,15 @@
 package tdd;
 
 public class Game {
+	
+	void roll(int nbQuilles) {
+		if(nbQuilles ==0) 
+			return 0;
+		
+	}
+	
+	int score() {
+		return 0;
+	}
 
 }
--- test/tdd/GameTest.java
+++ test/tdd/GameTest.java
@@ -1,5 +1,26 @@
 package tdd;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GameTest {
+	Game game;
+	
+	
+	@BeforeEach
+	void initGame() {
+		Game game = new Game();
+	}
+	
+	@Test
+	void aucuneQuille() {
+		for(int i =0; i<20; i++) {
+			game.roll(0);
+		}
+		assertEquals(0,game.score());
+	}
 
 }
